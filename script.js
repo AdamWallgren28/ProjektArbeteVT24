d0b2f888a7e092c38cb66ab3f093b82e489926fe
AdamWallgren28 <mattef91@hotmail.com>
2024-02-24 21:34:59 +0100

    html & css: cleans up toggle menu. js: activates both mailBtns

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-let burgerMenu = document.getElementById('burgerMenu');
+
+// toggleMeny
 let nav = document.querySelector('.navBox');
+let burgerMenu = document.getElementById('burgerMenu');
 let hemKnapp = document.getElementById('hemKnapp');
 let skillsKnapp = document.getElementById('skillsKnapp');
 let projectsKnapp = document.getElementById('projectsKnapp');
 let contactKnapp = document.getElementById('contactKnapp');
 let elements = [burgerMenu, hemKnapp, skillsKnapp, projectsKnapp, contactKnapp];
-
 
 function showHide() {
     if (nav.style.display === 'none' || nav.style.display === '') {
@@ -18,3 +19,9 @@ function showHide() {
 elements.forEach(element => {
     element.addEventListener('click', showHide);
 });
+
+// mailKnappar
+function openMail() {
+    var emailAddress = "mattef91@hotmail.com"; 
+    window.location.href = "mailto:" + emailAddress;
+}
